538d1e5bc574dc717ac7dd21652bdeb73a5885e8
sujit12x10 <sujit12x10@gmail.com>
2025-01-18 12:01:26 +0530

    create server for stripe

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+require("dotenv").config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Checkout API
+app.post("/api/create-checkout-session", async(req, res) => {
+    const {products} = req.body
+
+    const lineItems = products.map((product) => ({
+        price_data: {
+            currency: "inr",
+            product_data: {
+                name:product.name,
+                images: [product.image_url1]
+            },
+            unit_amount: product.price * 100
+        },
+        quantity: product.quantity
+    }))
+
+    const couponCode = req.body.coupon ? [{coupon: req.body.coupon}] : []
+    
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        mode: "payment",
+        success_url: "http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}",
+        cancel_url: "http://localhost:5173/cancel",  
+        discounts: couponCode,    
+    })
+
+    res.json({id: session.id})
+})
+
+app.listen(7000, () => {
+    console.log("server started")
+})
